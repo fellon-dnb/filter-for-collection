878f44fe373f419348919a11971849f3e402968e
fellon-dnb <fellon@ancevt.com>
2025-12-04 10:20:49 +0300

    feat: implement filter(T[], Filter) logic and add example usage

--- src/main/java/com/fellon/filtering/FilterUtils.java
+++ src/main/java/com/fellon/filtering/FilterUtils.java
@@ -2,6 +2,10 @@ package com.fellon.filtering;
 
 public class FilterUtils {
     public static <T>T[]filter(T[] array, Filter<T> filter){
-return  array;
+        T[] result = array.clone();
+        for (int i = 0; i < array.length; i++) {
+            result[i] = filter.apply(array[i]);
+        }
+        return result;
     }
 }
--- src/main/java/com/fellon/filtering/Main.java
+++ src/main/java/com/fellon/filtering/Main.java
@@ -1,13 +1,21 @@
 package com.fellon.filtering;
 
-/**
- * Hello world!
- *
- */
-public class Main
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        Integer[] numbers = {10,20,30,40,50,60,-10,-20,-30,-40};
+            Filter<Integer> x2 = new Filter<Integer>() {
+
+                @Override
+                public Integer apply(Integer o) {
+                    if (o == null) return null;
+                    return o * 2;
+                }
+            };
+            Integer[] result = FilterUtils.filter(numbers, x2);
+            System.out.println("до " + Arrays.toString(numbers));
+            System.out.println("после " + Arrays.toString(result));
+        }
+
     }
-}
